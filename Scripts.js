c81b94a8eba8c88692ba54d8326d29af58420c86
Alex <logicsofthemind@gmail.com>
2017-04-08 03:27:34 +0800

    add files

---
+++ Scripts.js
@@ -0,0 +1,214 @@
+
+$(".yeti").click(function(){
+
+  		alert("RAAAWR!");
+		$('.penguin1').css('background-image', 'url(Images/mound_1.png)');
+		$(".penguin1").mouseover(function() {
+			$(this).css('background-image', 'url(Images/mound_1_hover.png)');
+		});
+		$(".penguin1").mouseleave(function() {
+  		$(this).css('background-image', 'url(Images/mound_1.png)');    
+	    });
+
+	    $('.penguin2').css('background-image', 'url(Images/mound_2.png)');
+		$(".penguin2").mouseover(function() {
+			$(this).css('background-image', 'url(Images/mound_2_hover.png)');
+		});
+		$(".penguin2").mouseleave(function() {
+  		$(this).css('background-image', 'url(Images/mound_2.png)');    
+	    });
+
+	     $('.penguin3').css('background-image', 'url(Images/mound_3.png)');
+		$(".penguin3").mouseover(function() {
+			$(this).css('background-image', 'url(Images/mound_3_hover.png)');
+		});
+		$(".penguin3").mouseleave(function() {
+  		$(this).css('background-image', 'url(Images/mound_3.png)');    
+	    });
+
+	     $('.penguin4').css('background-image', 'url(Images/mound_4.png)');
+		$(".penguin4").mouseover(function() {
+			$(this).css('background-image', 'url(Images/mound_4_hover.png)');
+		});
+		$(".penguin4").mouseleave(function() {
+  		$(this).css('background-image', 'url(Images/mound_4.png)');    
+	    });
+
+	    $('.penguin5').css('background-image', 'url(Images/mound_5.png)');
+		$(".penguin5").mouseover(function() {
+			$(this).css('background-image', 'url(Images/mound_5_hover.png)');
+		});
+		$(".penguin5").mouseleave(function() {
+  		$(this).css('background-image', 'url(Images/mound_5.png)');    
+	    });
+
+
+	    $('.penguin6').css('background-image', 'url(Images/mound_6.png)');
+		$(".penguin6").mouseover(function() {
+			$(this).css('background-image', 'url(Images/mound_6_hover.png)');
+		});
+		$(".penguin6").mouseleave(function() {
+  		$(this).css('background-image', 'url(Images/mound_6.png)');    
+	    });
+
+	    $('.penguin7').css('background-image', 'url(Images/mound_7.png)');
+		$(".penguin7").mouseover(function() {
+			$(this).css('background-image', 'url(Images/mound_7_hover.png)');
+		});
+		$(".penguin7").mouseleave(function() {
+  		$(this).css('background-image', 'url(Images/mound_7.png)');    
+	    });
+
+	    $('.penguin8').css('background-image', 'url(Images/mound_8.png)');
+		$(".penguin8").mouseover(function() {
+			$(this).css('background-image', 'url(Images/mound_8_hover.png)');
+		});
+		$(".penguin8").mouseleave(function() {
+  		$(this).css('background-image', 'url(Images/mound_8.png)');    
+	    });
+ 		    
+   });
+  
+
+$( ".penguin1" ).mouseleave(function() {
+  	$(this).css('background-image', 'url(Images/mound_1.png)');    
+  	});
+
+
+$(".penguin1").click(function(){
+	$(this).css('background-image', 'url(Images/penguin_1.png)');
+ 	$(this).unbind('mouseleave');
+ 	$(this).unbind('mouseover');
+});
+
+$(".penguin1").mouseover(function() {
+  	$(this).css('background-image', 'url(Images/mound_1_hover.png)');    
+  });
+
+$(".penguin1").mouseleave(function() {
+  	$(this).css('background-image', 'url(Images/mound_1.png)');    
+  });
+
+$( ".penguin2" ).mouseleave(function() {
+  	$(this).css('background-image', 'url(Images/mound_2.png)');    
+  	});
+
+
+$(".penguin2").click(function(){
+	$(this).css('background-image', 'url(Images/penguin_2.png)');
+ 	$(this).unbind('mouseleave');
+ 	$(this).unbind('mouseover');
+});
+
+$(".penguin2").mouseover(function() {
+  	$(this).css('background-image', 'url(Images/mound_2_hover.png)');    
+  });
+
+$(".penguin2").mouseleave(function() {
+  	$(this).css('background-image', 'url(Images/mound_2.png)');    
+  });
+
+$( ".penguin3" ).mouseleave(function() {
+  	$(this).css('background-image', 'url(Images/mound_3.png)');    
+  	});
+
+
+$(".penguin3").click(function(){
+	$(this).css('background-image', 'url(Images/penguin_3.png)');
+ 	$(this).unbind('mouseleave');
+ 	$(this).unbind('mouseover');
+});
+
+$(".penguin3").mouseover(function() {
+  	$(this).css('background-image', 'url(Images/mound_3_hover.png)');    
+  });
+
+$(".penguin3").mouseleave(function() {
+  	$(this).css('background-image', 'url(Images/mound_3.png)');    
+  });
+
+$( ".penguin4" ).mouseleave(function() {
+  	$(this).css('background-image', 'url(Images/mound_4.png)');    
+  	});
+
+
+$(".penguin4").click(function(){
+	$(this).css('background-image', 'url(Images/penguin_4.png)');
+ 	$(this).unbind('mouseleave');
+ 	$(this).unbind('mouseover');
+});
+
+$(".penguin4").mouseover(function() {
+  	$(this).css('background-image', 'url(Images/mound_4_hover.png)');    
+  });
+
+$(".penguin4").mouseleave(function() {
+  	$(this).css('background-image', 'url(Images/mound_4.png)');    
+  });
+
+$( ".penguin5" ).mouseleave(function() {
+  	$(this).css('background-image', 'url(Images/mound_5.png)');    
+  	});
+
+
+$(".penguin5").click(function(){
+	$(this).css('background-image', 'url(Images/penguin_5.png)');
+ 	$(this).unbind('mouseleave');
+ 	$(this).unbind('mouseover');
+});
+
+$(".penguin5").mouseover(function() {
+  	$(this).css('background-image', 'url(Images/mound_5_hover.png)');    
+  });
+
+$(".penguin5").mouseleave(function() {
+  	$(this).css('background-image', 'url(Images/mound_5.png)');    
+  });
+
+$( ".penguin6" ).mouseleave(function() {
+  	$(this).css('background-image', 'url(Images/mound_6.png)');    
+  	});
+
+
+$(".penguin6").click(function(){
+	$(this).css('background-image', 'url(Images/penguin_6.png)');
+ 	$(this).unbind('mouseleave');
+ 	$(this).unbind('mouseover');
+});
+
+$(".penguin6").mouseover(function() {
+  	$(this).css('background-image', 'url(Images/mound_6_hover.png)');    
+  });
+
+$(".penguin6").mouseleave(function() {
+  	$(this).css('background-image', 'url(Images/mound_6.png)');    
+  });
+
+$(".penguin7").click(function(){
+	$(this).css('background-image', 'url(Images/penguin_7.png)');
+ 	$(this).unbind('mouseleave');
+ 	$(this).unbind('mouseover');
+});
+
+$(".penguin7").mouseover(function() {
+  	$(this).css('background-image', 'url(Images/mound_7_hover.png)');    
+  });
+
+$(".penguin7").mouseleave(function() {
+  	$(this).css('background-image', 'url(Images/mound_7.png)');    
+  });
+
+$(".penguin8").click(function(){
+	$(this).css('background-image', 'url(Images/penguin_8.png)');
+ 	$(this).unbind('mouseleave');
+ 	$(this).unbind('mouseover');
+});
+
+$(".penguin8").mouseover(function() {
+  	$(this).css('background-image', 'url(Images/mound_8_hover.png)');    
+  });
+
+$(".penguin8").mouseleave(function() {
+  	$(this).css('background-image', 'url(Images/mound_8.png)');    
+  });
+
